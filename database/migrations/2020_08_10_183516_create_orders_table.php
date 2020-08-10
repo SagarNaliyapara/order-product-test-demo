06d8a5c2fc987e7d266514f7880bd960f9aadbca
Sagar Naliyapara <sagar.naliyapara@noovosoft.com>
2020-08-11 00:10:20 +0530

    Create project and add migrations

---
+++ database/migrations/2020_08_10_183516_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('shipping_address_1');
+            $table->string('shipping_address_2');
+            $table->string('shipping_address_3');
+            $table->string('city');
+            $table->string('country_code');
+            $table->string('zip_postal_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
